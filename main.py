37c2baaace46a66a5be0fe7bcfd379886245da22
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-13 11:41:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from words import words
+import random
+from visual_hangman import visual_lives
 
+
+def valid_words():
+    random_word = random.choice(words)
+
+    while " " in random_word and "-" in random_word:
+        random_word = random.choice(words)
+
+    return random_word.upper()
